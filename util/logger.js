2d09db13233ec3b1be035191e6ed46aa266de7c3
Brenton McSweyn <brenton.mcsweyn@gmail.com.au>
2015-12-11 10:57:21 +1100

    moving to redis to store jobs

---
+++ util/logger.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+  var winston = require('winston');
+  var logger = new(winston.Logger)({
+    transports: [
+      new(winston.transports.Console)({
+        colorize: 'all',
+        timestamp: true      
+      }),
+      new(winston.transports.File)({
+        filename: 'logs/output.log',
+
+      }),
+    ],
+    exceptionHandlers: [
+      new(winston.transports.Console)({
+        colorize: 'all',
+      }),
+      new winston.transports.File({
+        filename: 'logs/exceptions.log',
+      }),
+    ],
+  });
+
+  module.exports = logger;
+})();
